5c3ca49dd730c62c1046b648e0e0a7aae61e4904
Igor <mr.greatanderson@gmail.com>
2020-06-07 22:20:35 +0300

    git commit -a -m

--- task_1.js
+++ task_1.js
@@ -15,13 +15,42 @@
 
 
 //  Найти sum
-
-let numbers = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+/*
+let numbers = [ -1,-8,-2];
 let sum = 0; 
 for (let i = 0; i < numbers.length; i++) { 
   if (numbers[i] > 0) {
-	  sum += numbers[i]; 
-  }    
+      sum += numbers[i]; 
+    
+  } 
+
 }
 console.log(sum);
+*/
+
+
+//Найти max
+let maxSum = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+    for (let i = 0; i < maxSum.length; i++) {
+        if(maxSum[i] > maxSum[0]){
+            maxSum[0] = maxSum[i];
+    }
+}
+console.log(maxSum[0]);
+
+
+//Найти min
+let minSum = [1,NaN,3,5,-3];
+    for (let i = 0; i < minSum.length; i++) {
+        if(minSum[i] < minSum[0]){
+            minSum[0] = minSum[i];
+    }
+}
+console.log(minSum[0]);
+
+
+
+
+
+
 
