bd30371f19d17f39a0c0462e91849db5097ba215
Igor <mr.greatanderson@gmail.com>
2020-06-11 13:15:41 +0300

    add check typeof

--- task_1.js
+++ task_1.js
@@ -16,35 +16,44 @@
 
 
 //  Найти sum
-let num = [-5,5,true,null];
+let num = [-5,5,true,null, Infinity];
 let sum = 0; 
 for (let i = 0, len = num.length; i < len; i++) {
     
-    if ( typeof num[i] === 'number' ) {
+if(num[i] === Infinity) continue;
+else if ( typeof num[i] === 'number' ) {
         sum += num[i]; 
     }
 }
-console.log( sum ); 
+console.log( sum );
 
 
 //Найти max
-let maxSum = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-    for (let i = 0; i < maxSum.length; i++) {
-        if(maxSum[i] > maxSum[0]){
-            maxSum[0] = maxSum[i];
-    }
+maxSum = [-5,5,true,null, false]; 
+
+for (let i = 1; i < maxSum.length; i++) { 
+
+if(maxSum[i] === Infinity ||  typeof maxSum[i] !== 'number' || 
+   maxSum[i] < maxSum[0]) continue;
+
+ maxSum[0] = maxSum[i]; 
 }
 console.log(maxSum[0]);
 
 
 //Найти min
-let minSum = [1,undefined,3,5,-3];
-    for (let i = 0; i < minSum.length; i++) {
-        if(minSum[i] < minSum[0]){
-            minSum[0] = minSum[i];
-    }
+minSum = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1, false]; 
+
+for (let i = 1; i < minSum.length; i++) { 
+
+if(minSum[i] === Infinity ||  typeof minSum[i] !== 'number' || 
+   minSum[i] > minSum[0]) continue;
+
+ minSum[0] = minSum[i]; 
 }
 console.log(minSum[0]);
+
+
 
 
 
